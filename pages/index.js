d61edb992b515ccd1de9bf913fe670b97fbba0e2
Dakota Jones <dakota32jones@gmail.com>
2022-11-05 19:12:49 -0500

    added styling to navbar

--- pages/index.js
+++ pages/index.js
@@ -13,13 +13,18 @@ export default function Home() {
       <main className=" bg-white px-10">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1>Developed by Dakota Jones</h1>
-            <ul>
+            <h1 className="text-xl font-burtons">Developed by Dakota Jones</h1>
+            <ul className=" flex items-center">
               <li>
-                <BsFillMoonStarsFill />
+                <BsFillMoonStarsFill className=" cursor-pointer text-2xl" />
               </li>
               <li>
-                <a href="#">Resume</a>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8 "
+                  href="#"
+                >
+                  Resume
+                </a>
               </li>
             </ul>
           </nav>
